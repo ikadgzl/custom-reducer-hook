1408ab8eadd4dcf9bef028e0cdf4af412c74d356
ikadgzl <ikadgzl@gmail.com>
2021-08-07 05:33:18 +0300

    built my own useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const INITIAL_STATE = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + 1 };
+    case 'DECREMENT':
+      return { ...state, count: state.count - 1 };
+
+    default:
+      break;
+  }
+};
+
+const useReducerHook = (reducer, initialState) => {
+  const [state, setState] = useState(initialState);
+
+  const dispatch = (action) => {
+    const nextState = reducer(state, action);
+    setState(nextState);
+  };
+
+  return [state, dispatch];
+};
 
 function App() {
+  const [state, dispatch] = useReducerHook(reducer, INITIAL_STATE);
+
+  const incrementNumber = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  const decrementNumber = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>Count: {state.count}</div>
+
+      <button onClick={incrementNumber}>INCREMENT</button>
+      <button onClick={decrementNumber}>DECREMENT</button>
     </div>
   );
 }
